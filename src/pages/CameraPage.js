9032e64b667a0a89ff169af4effeedaddf01b81c
boafow <boafowill@gmail.com>
2023-02-15 12:08:59 -0500

    Remove remote src

--- src/pages/CameraPage.js
+++
@@ -1,9 +0,0 @@
-import { React } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import PhoneCamera from '../components/PhoneCamera';
-
-export default CameraPage = () => {
-    return (
-        <PhoneCamera />
-    );
-}
