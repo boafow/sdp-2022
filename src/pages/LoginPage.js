6f7721cf6205f069fdd1485c976289c7f6696c8a
boafow <boafowill@gmail.com>
2023-02-15 12:10:51 -0500

    Remove duplicate components folder

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import Login from '../components/Login';
+
+export default LoginPage = () => {
+    return (
+        <Login />
+    );
+}
+    ;
