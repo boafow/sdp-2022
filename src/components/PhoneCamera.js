cc5aadd828e1a673d0b02ddcbbbef3f504e2fb78
Ali <alhamdaniali10@gmail.com>
2023-02-17 15:39:06 -0500

    Fixed the upload to S3, can successfully verify in broswer, also fixed issue of saving a duplicate of the image selected

--- src/components/PhoneCamera.js
+++ src/components/PhoneCamera.js
@@ -15,11 +15,17 @@ export default PhoneCamera = () => {
     const randomNumber = Math.floor(Math.random() * 1000000);
     return `${timestamp}-${randomNumber}.jpeg`;
   };
-  const uploadPicture = async ( picture ) => {
+  const fetchImageFromUri = async (uri) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    return blob;
+  };
+  const uploadPicture = async ( uri ) => {
     const key = generatePictureKey();
-    console.log("Pic: " + picture);
+    console.log("PicUri: " + uri);
+    const img = await fetchImageFromUri(uri);
     try {
-      const result = await Storage.put(key, picture, {
+      const result = await Storage.put(key, img, {
         contentType: 'image/jpeg'
       });
       console.log(result);
@@ -35,13 +41,12 @@ export default PhoneCamera = () => {
     if (!granted) {
       return;
     }
-  
-    const picture = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    });
+  // {mediaTypes: ImagePicker.MediaTypeOptions.Images,}
+    const picture = await ImagePicker.launchImageLibraryAsync();
     if (!picture.cancelled) {
-      const asset = await MediaLibrary.createAssetAsync(picture.uri );
-      const fileUri = asset.uri;
+      //const asset = await MediaLibrary.createAssetAsync(picture.uri );
+      //const fileUri = asset.uri;
+      const fileUri = picture.uri;
   
       const uploadedPictureKey = await uploadPicture(fileUri);
       console.log(`The picture was uploaded with the key: ${uploadedPictureKey}`);
@@ -49,7 +54,7 @@ export default PhoneCamera = () => {
   };
   
   let cameraRef = useRef();
-  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasCameraPermission, setHasCameraPermission] = useState();  
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
   const [photo, setPhoto] = useState();
 
@@ -85,12 +90,13 @@ export default PhoneCamera = () => {
         setPhoto(undefined);
       });
     };
-
+    
     let savePhoto = () => {
       MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
         setPhoto(undefined);
       });
     };
+    
 
     return (
       <SafeAreaView style={styles.container}>
@@ -115,7 +121,7 @@ export default PhoneCamera = () => {
       <StatusBar style="auto" />
     </Camera>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
