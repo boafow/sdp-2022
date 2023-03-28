97cd968e1d99862d88e349bd04f5f6ccf950ec0a
Neil Thakur <56324725+neil-thakur@users.noreply.github.com>
2023-03-28 00:44:58 -0400

    Had to scrap Will's code and create my own screen using react-native-dropdown-picker; I can select age, height-ft, height-in and log to console using button. So far so good. Can implement Jorge's API soon.

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,173 +1,109 @@
+import { style } from 'deprecated-react-native-prop-types/DeprecatedViewPropTypes';
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, SafeAreaView } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import { View, Text, StyleSheet, TextInput, SafeAreaView, Button } from 'react-native';
+//import { Picker } from '@react-native-picker/picker';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 export default ProfileScreen = () => {
-  const [feet, setFeet] = useState('5');
-  const [inches, setInches] = useState('6');
-  const [weight, setWeight] = useState('150');
-  const [age, setAge] = useState('25');
-  const [dietaryRestrictions, setDietaryRestrictions] = useState('None');
-  const [Calories, setCalories] = useState('');
-  const [protein, setProtein] = useState('');
-  const [carbs, setCarbs] = useState('');
-  const [fats, setFats] = useState('');
-  const [exerices, setExerices] = useState('');
+    //GENERATE AGE ARRAY
+    const ageItems = [];
+    for (let i = 18; i <= 75; i++) {
+        ageItems.push({ label: `${i}`, value: i });
+    }
 
-  return (
-    <SafeAreaView>
-        <View style={styles.container}>
-        {/* HEIGHT */}
-        <Text style={styles.label}>Height:</Text>
-        <View style={styles.pickerContainer}> 
-            <Picker
-            style={styles.picker}
-            selectedValue={feet}
-            onValueChange={setFeet}
-            mode={'dropdown'}
-            >
-            {Array.from({ length: 8 }, (_, i) => i + 3).map((value) => (
-                <Picker.Item key={value} label={`${value} ft`} value={value.toString()} />
-            ))}
-            </Picker>
+    //GENERATE HEIGHT FEET ARRAY
+    const heightFeetItems = [];
+    for (let i = 1; i <= 7; i++) {
+        heightFeetItems.push({ label: `${i} ft`, value: i });
+    }
+    //GENERATE HEIGHT INCHES ARRAY
+    const heightInchesItems = [];
+    for (let i = 0; i <= 11; i++) {
+        heightInchesItems.push({ label: `${i} in`, value: i });
+    }
 
-            <Picker
-            style={styles.picker}
-            selectedValue={inches}
-            onValueChange={setInches}
-            >
-            {Array.from({ length: 12 }, (_, i) => i).map((value) => (
-                <Picker.Item key={value} label={`${value} in`} value={value.toString()} />
-            ))}
-            </Picker>
-        </View>
 
-        {/* HEIGHT */}
-        <Text style={styles.label}>Weight (lbs):</Text>
-        <View style={styles.pickerContainer}>
-            <Picker
-                style={styles.picker}
-                selectedValue={weight}
-                onValueChange={setWeight}
-            >
-                {Array.from({ length: 300 }, (_, i) => i + 50).map((value) => (
-                <Picker.Item key={value} label={value.toString()} value={value.toString()} />
-                ))}
-            </Picker>
-        </View>
-        
-        {/* AGE */}
-        <Text style={styles.label}>Age:</Text>
-        <View style={styles.pickerContainer}>   
-            <Picker
-                style={styles.picker}
-                selectedValue={age}
-                onValueChange={setAge}
-            >
-                {Array.from({ length: 100 }, (_, i) => i + 10).map((value) => (
-                <Picker.Item key={value} label={value.toString()} value={value.toString()} />
-                ))}
-            </Picker>
-        </View>
+    const [openAge, setOpenAge] = useState(false);
+    const [openHeightFeet, setOpenHeightFeet] = useState(false);
+    const [openHeightInches, setOpenHeightInches] = useState(false);
 
-        {/* DIETARY RESTRICTIONS */}
-        <Text style={styles.label}>Dietary Restrictions:</Text>
-        <View style={styles.pickerContainer}>
-            <Picker
-                style={styles.picker}
-                selectedValue={dietaryRestrictions}
-                onValueChange={setDietaryRestrictions}
-            >
-                <Picker.Item label="None" value="None" />
-                <Picker.Item label="Vegan" value="Vegan" />
-                <Picker.Item label="Vegetarian" value="Vegetarian" />
-                <Picker.Item label="Peanut Allergy" value="Peanut Allergy" />
-                <Picker.Item label="Lactose Intolerance" value="Lactose Intolerance" />
-                <Picker.Item label="Gluten Intolerance" value="Gluten Intolerance" />
-                <Picker.Item label="Dairy-Free" value="Dairy-Free" />
-            </Picker>
-        </View>
+    const [valueAge, setValueAge] = useState(null);
+    const [itemsAge, setItemsAge] = useState(ageItems);
 
-        <Text style={styles.label}>Calories:</Text>
-        <TextInput
-            style={styles.input}
-            value={Calories}
-            onChangeText={setCalories}
-            keyboardType="numeric"
-            placeholder="Enter your calories goal for the day"
-        />
-        <Text style={styles.label}>Protein:</Text>
-        <TextInput
-            style={styles.input}
-            value={protein}
-            onChangeText={setProtein}
-            keyboardType="numeric"
-            placeholder="Enter your protein goal"
-        />
-        <Text style={styles.label}>Carbs:</Text>
-        <TextInput
-            style={styles.input}
-            value={carbs}
-            onChangeText={setCarbs}
-            keyboardType="numeric"
-            placeholder="Enter your Carbs goal"
-        />
-        <Text style={styles.label}>Fats:</Text>
-        <TextInput
-            style={styles.input}
-            value={fats}
-            onChangeText={setFats}
-            keyboardType="numeric"
-            placeholder="Enter your Fat goal"
-        />
-        <Text style={styles.label}>Exerices:</Text>
-        <TextInput
-            style={styles.input}
-            value={exerices}
-            onChangeText={setExerices}
-            keyboardType="numeric"
-            placeholder="Enter your exericse goal"
-        />
-            
-        </View>
-    </SafeAreaView>
-    
-  );
+    const [valueHeightFeet, setValueHeightFeet] = useState(null);
+    const [itemsHeightFeet, setItemsHeightFeet] = useState(heightFeetItems);
+
+    const [valueHeightInches, setValueHeightInches] = useState(null);
+    const [itemsHeightInches, setItemHeightInches] = useState(heightInchesItems);
+
+    return (
+        <SafeAreaView>
+            <Text style={styles.text}>Age:</Text>
+            <DropDownPicker
+                open={openAge}
+                value={valueAge}
+                items={itemsAge}
+                setOpen={setOpenAge}
+                setValue={setValueAge}
+                setItems={setItemsAge}
+                placeholder={'Select your age'}
+            />
+            <Text style={styles.text}>Height:</Text>
+            <View style={{flexDirection: 'row'}}>
+                <View style={{ flex: 1, width: '50%' }}>
+                    <DropDownPicker
+                        open={openHeightFeet}
+                        value={valueHeightFeet}
+                        items={itemsHeightFeet}
+                        setOpen={setOpenHeightFeet}
+                        setValue={setValueHeightFeet}
+                        setItems={setItemsHeightFeet}
+                        placeholder={'Select your height (ft)'}
+                    />
+                </View>
+                <View style={{ flex: 1, width: '50%' }}>
+                    <DropDownPicker
+                        open={openHeightInches}
+                        value={valueHeightInches}
+                        items={itemsHeightInches}
+                        setOpen={setOpenHeightInches}
+                        setValue={setValueHeightInches}
+                        setItems={setItemHeightInches}
+                        placeholder={'Select your height (in)'}
+                    />
+                </View>
+            </View>
+            <Button 
+                title='Log to Console'
+                onPress={() => {console.log(valueAge, valueHeightFeet, valueHeightInches);}}
+            >
+            </Button>
+        </SafeAreaView>
+    );
+
 };
+
 const styles = StyleSheet.create({
-  container: {
-    padding: 0,
-    backgroundColor: '#fff',
-  },
-  label: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  pickerContainer: {
-    flexDirection: 'row',
-    marginTop: 5,
-    marginBottom: 5,
-    borderColor: 'black',
-    borderWidth: 2,
-    overflow: 'hidden'
-  },
-  picker: {
-    flex: 1,
-    width: 10, 
-    height: 100,
-    numberOfLines: 1
-  },
-  input: {
-    height: 30,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 5,
-    marginBottom: 1,
-  },
-});
-
-
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'left',
+      paddingTop: 5,
+      paddingBottom: 5
+    },
+    pickerContainer: {
+        flexDirection: 'row',
+        marginTop: 5,
+        marginBottom: 5,
+        borderColor: 'black',
+        borderWidth: 2,
+        overflow: 'hidden'
+      },
+  });
